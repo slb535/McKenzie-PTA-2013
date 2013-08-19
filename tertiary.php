bb8c72fac1e752ea2f0e54befde70626955221a9
SBarGot <gottlist@comcast.net>
2013-08-19 14:23:38 -0500

    I believe everything works at this point.

--- tertiary.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo '<div id="nav"><div>';
-wp_nav_menu(array('sort_column' => 'menu_order', 'container_id' => 'tertiary', 'menu_class' => 'menu genesis-nav-menu menu-tertiary', 'theme_location' => 'tertiary'));
-echo '</div></div>';
-?>
